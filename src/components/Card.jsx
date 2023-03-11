817c832b779799cd365897e6d50ff2ec8b5196a1
EAGLE1309 <vighneshbangar57@gmail.com>
2023-03-11 20:15:53 +0530

    [UPDATE] Added Popular Residence Section!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,67 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Grid, Image, Icon } from '@chakra-ui/react';
 import Text from './Text.jsx';
+import Button from './Button.jsx';
+import Heading from './Heading.jsx';
+import {
+  IoLocationSharp,
+  IoBedSharp,
+  IoExpandSharp,
+  IoScanSharp
+} from 'react-icons/io5';
 
-export default function Card({ text, value }) {
+export default function Card({ image, address, bhk, dimensions, area, price }) {
   return (
-    <Flex border="2px solid gray" bg="white" direction="column" mb={4}>
+    <Flex bg="white" direction="column" mb={4}>
       <Image
-        src="/house-7.png"
-        minW={{ base: '400px', md: '275px' }}
-        minH={{ base: 'auto', md: '275px' }}
-        maxW={{ base: '400px', md: '275px' }}
-        maxH={{ base: 'auto', md: '275px' }}
+        src={image}
+        minW={{ base: 'calc(100vw - 3rem)', md: '100%' }}
+        minH={{ base: 'calc(100vw - 6rem)', md: '325px' }}
+        maxW={{ base: 'calc(100vw - 3rem)', md: '100%' }}
+        maxH={{ base: 'calc(100vw - 8rem)', md: '325px' }}
         objectFit="cover"
         alt="home image"
       />
-      <Flex align="start" justify="start" p={{ base: 4, '2xl': 6 }}>
-        <Text color="gray.600" fontWeight="500">
-          Telco Colony, Rajgurunagar
-        </Text>
-        <Text color="gray.600" fontWeight="500">
-          8 x 8 m
-        </Text>
+      <Flex
+        direction="column"
+        align="start"
+        justify="start"
+        p={{ base: 6, md: 8, '2xl': 10 }}>
+        <Flex mb={{ base: 4, md: 7 }} wrap="wrap" align="start" justify="start">
+          <InfoText mb={3} flexBasis="100%" icon={IoLocationSharp}>
+            {address}
+          </InfoText>
+          <InfoText mr={4} icon={IoBedSharp}>
+            {bhk + ' BHK'}
+          </InfoText>
+          <InfoText mr={4} icon={IoScanSharp}>
+            {dimensions}
+          </InfoText>
+          <InfoText icon={IoExpandSharp}>{area}</InfoText>
+        </Flex>
+        <Grid
+          gap={{ md: 6 }}
+          w="100%"
+          placeItems="center"
+          templateColumns="repeat(2, 1fr)">
+          <Button whiteSpace="nowrap" w="100%">
+            Book now
+          </Button>
+          <Heading as="h4" fontWeight="semibold">
+            {price}
+          </Heading>
+        </Grid>
       </Flex>
     </Flex>
   );
 }
+
+const InfoText = (props) => {
+  return (
+    <Flex {...props} direction="row" align="center">
+      <Icon as={props.icon} mb={'0.1rem'} mr={1.5} />
+      <Text whiteSpace="nowrap" color="gray.600" fontWeight="500">
+        {props.children}
+      </Text>
+    </Flex>
+  );
+};
--- src/sections/Popular.jsx
+++ src/sections/Popular.jsx
@@ -1,33 +1,84 @@
-import { chakra, Flex, Grid } from '@chakra-ui/react';
+import { chakra, Flex } from '@chakra-ui/react';
 import { Text, Heading, Card } from '../components';
+
+const estates = [
+  {
+    image: 'house-2.png',
+    address: '3865 Southern Avenue, Weldon',
+    bhk: 4,
+    dimensions: '10x10 m',
+    area: '1800 m²',
+    price: '$250,000'
+  },
+  {
+    image: 'house-3.png',
+    address: '1823 Polk Street, Tucson',
+    bhk: 5,
+    dimensions: '14x14 m',
+    area: '1600 m²',
+    price: '$300,000'
+  },
+  {
+    image: 'house-4.png',
+    address: '2977 Henry Ford Avenue, Tulsa',
+    bhk: 3,
+    dimensions: '12x12 m',
+    area: '2000 m²',
+    price: '$180,000'
+  }
+];
 
 export default function Popular() {
   const sectionGap = { base: 16, md: 24, lg: 28 };
 
   return (
-    <Flex pb={sectionGap} w="100%" direction="column" align="start">
-      <Flex pb={{ base: 2, md: 4, lg: 6 }} direction="row" align="center">
-        <chakra.span
-          mr={2}
-          w={{ base: 8, md: 10, lg: 12 }}
-          h={'1.25px'}
-          bg="gray.800"
-        />
-        <Text>POPULAR</Text>
-      </Flex>
-      <Heading pb={6} fontWeight="semibold" as="h3">
-        Our Popular Residence
-      </Heading>
+    <Flex
+      _after={{
+        content: "''",
+        position: 'absolute',
+        top: '50%',
+        width: '150%',
+        height: '1000px',
+        bg: 'gray.800',
+        zIndex: '-1'
+      }}
+      position="relative"
+      w="100%"
+      direction="column"
+      align="center">
+      <Flex pb={sectionGap} w="100%" direction="column" align="start">
+        <Flex pb={{ base: 2, md: 4, lg: 6 }} direction="row" align="center">
+          <chakra.span
+            mr={2}
+            w={{ base: 8, md: 10, lg: 12 }}
+            h={'1.25px'}
+            bg="gray.800"
+          />
+          <Text>POPULAR</Text>
+        </Flex>
+        <Heading pb={6} fontWeight="semibold" as="h3">
+          Our Popular Residence
+        </Heading>
 
-      <Grid
-        w="100%"
-        gap={6}
-        overflow="scroll"
-        templateColumns={{ base: 'repeat(3, 1fr)' }}>
-        <Card />
-        <Card />
-        <Card />
-      </Grid>
+        <Flex
+          w="100%"
+          className="no__overflow-scroll"
+          gap={{ base: 6, md: 12 }}
+          overflow="scroll"
+          position="relative">
+          {estates &&
+            estates.map((data) => (
+              <Card
+                image={data.image}
+                address={data.address}
+                bhk={data.bhk}
+                dimensions={data.dimensions}
+                area={data.area}
+                price={data.price}
+              />
+            ))}
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
